4c3d2ece539191d8a72da87ba40535b4d892aa1e
Matheus Vieira <mapivieira@gmail.com>
2024-11-01 00:08:01 +0000

    lista methods

---
+++ 2quest.js
@@ -0,0 +1,6 @@
+const colors = ['red','blue','green'];
+
+colors.shift()
+colors.unshift('purple')
+
+console.log(colors)
---
+++ 3quest.js
@@ -0,0 +1,6 @@
+const numbers = [1,2,3];
+const numeros = [4,5,6];
+
+let novoNumbers = numbers.concat(numeros);
+
+console.log(novoNumbers)
